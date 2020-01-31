d7b0b146b0ee78cb69ae8350d99b8e791f01d3c2
Matt Norman <normanmr@ornl.gov>
2020-01-30 22:16:59 -0500

    Switching to fortran-like FArrays

--- const.h
+++ const.h
@@ -2,83 +2,83 @@
 #pragma once
 
 #include "YAKL.h"
-#include "Array.h"
+#include "FArray.h"
 #include "SArray.h"
 
 typedef double real;
 
 
-typedef yakl::Array<real,yakl::memDevice> umgReal1d;
-typedef yakl::Array<real,yakl::memDevice> umgReal2d;
-typedef yakl::Array<real,yakl::memDevice> umgReal3d;
-typedef yakl::Array<real,yakl::memDevice> umgReal4d;
-typedef yakl::Array<real,yakl::memDevice> umgReal5d;
-typedef yakl::Array<real,yakl::memDevice> umgReal6d;
-typedef yakl::Array<real,yakl::memDevice> umgReal7d;
+typedef yakl::FArray<real,yakl::memDevice> umgReal1d;
+typedef yakl::FArray<real,yakl::memDevice> umgReal2d;
+typedef yakl::FArray<real,yakl::memDevice> umgReal3d;
+typedef yakl::FArray<real,yakl::memDevice> umgReal4d;
+typedef yakl::FArray<real,yakl::memDevice> umgReal5d;
+typedef yakl::FArray<real,yakl::memDevice> umgReal6d;
+typedef yakl::FArray<real,yakl::memDevice> umgReal7d;
 
-typedef yakl::Array<real,yakl::memDevice> real1d;
-typedef yakl::Array<real,yakl::memDevice> real2d;
-typedef yakl::Array<real,yakl::memDevice> real3d;
-typedef yakl::Array<real,yakl::memDevice> real4d;
-typedef yakl::Array<real,yakl::memDevice> real5d;
-typedef yakl::Array<real,yakl::memDevice> real6d;
-typedef yakl::Array<real,yakl::memDevice> real7d;
+typedef yakl::FArray<real,yakl::memDevice> real1d;
+typedef yakl::FArray<real,yakl::memDevice> real2d;
+typedef yakl::FArray<real,yakl::memDevice> real3d;
+typedef yakl::FArray<real,yakl::memDevice> real4d;
+typedef yakl::FArray<real,yakl::memDevice> real5d;
+typedef yakl::FArray<real,yakl::memDevice> real6d;
+typedef yakl::FArray<real,yakl::memDevice> real7d;
 
-typedef yakl::Array<real,yakl::memHost> realHost1d;
-typedef yakl::Array<real,yakl::memHost> realHost2d;
-typedef yakl::Array<real,yakl::memHost> realHost3d;
-typedef yakl::Array<real,yakl::memHost> realHost4d;
-typedef yakl::Array<real,yakl::memHost> realHost5d;
-typedef yakl::Array<real,yakl::memHost> realHost6d;
-typedef yakl::Array<real,yakl::memHost> realHost7d;
+typedef yakl::FArray<real,yakl::memHost> realHost1d;
+typedef yakl::FArray<real,yakl::memHost> realHost2d;
+typedef yakl::FArray<real,yakl::memHost> realHost3d;
+typedef yakl::FArray<real,yakl::memHost> realHost4d;
+typedef yakl::FArray<real,yakl::memHost> realHost5d;
+typedef yakl::FArray<real,yakl::memHost> realHost6d;
+typedef yakl::FArray<real,yakl::memHost> realHost7d;
 
-typedef yakl::Array<int,yakl::memDevice> umgInt1d;
-typedef yakl::Array<int,yakl::memDevice> umgInt2d;
-typedef yakl::Array<int,yakl::memDevice> umgInt3d;
-typedef yakl::Array<int,yakl::memDevice> umgInt4d;
-typedef yakl::Array<int,yakl::memDevice> umgInt5d;
-typedef yakl::Array<int,yakl::memDevice> umgInt6d;
-typedef yakl::Array<int,yakl::memDevice> umgInt7d;
+typedef yakl::FArray<int,yakl::memDevice> umgInt1d;
+typedef yakl::FArray<int,yakl::memDevice> umgInt2d;
+typedef yakl::FArray<int,yakl::memDevice> umgInt3d;
+typedef yakl::FArray<int,yakl::memDevice> umgInt4d;
+typedef yakl::FArray<int,yakl::memDevice> umgInt5d;
+typedef yakl::FArray<int,yakl::memDevice> umgInt6d;
+typedef yakl::FArray<int,yakl::memDevice> umgInt7d;
 
-typedef yakl::Array<int,yakl::memDevice> int1d;
-typedef yakl::Array<int,yakl::memDevice> int2d;
-typedef yakl::Array<int,yakl::memDevice> int3d;
-typedef yakl::Array<int,yakl::memDevice> int4d;
-typedef yakl::Array<int,yakl::memDevice> int5d;
-typedef yakl::Array<int,yakl::memDevice> int6d;
-typedef yakl::Array<int,yakl::memDevice> int7d;
+typedef yakl::FArray<int,yakl::memDevice> int1d;
+typedef yakl::FArray<int,yakl::memDevice> int2d;
+typedef yakl::FArray<int,yakl::memDevice> int3d;
+typedef yakl::FArray<int,yakl::memDevice> int4d;
+typedef yakl::FArray<int,yakl::memDevice> int5d;
+typedef yakl::FArray<int,yakl::memDevice> int6d;
+typedef yakl::FArray<int,yakl::memDevice> int7d;
 
-typedef yakl::Array<int,yakl::memHost> intHost1d;
-typedef yakl::Array<int,yakl::memHost> intHost2d;
-typedef yakl::Array<int,yakl::memHost> intHost3d;
-typedef yakl::Array<int,yakl::memHost> intHost4d;
-typedef yakl::Array<int,yakl::memHost> intHost5d;
-typedef yakl::Array<int,yakl::memHost> intHost6d;
-typedef yakl::Array<int,yakl::memHost> intHost7d;
+typedef yakl::FArray<int,yakl::memHost> intHost1d;
+typedef yakl::FArray<int,yakl::memHost> intHost2d;
+typedef yakl::FArray<int,yakl::memHost> intHost3d;
+typedef yakl::FArray<int,yakl::memHost> intHost4d;
+typedef yakl::FArray<int,yakl::memHost> intHost5d;
+typedef yakl::FArray<int,yakl::memHost> intHost6d;
+typedef yakl::FArray<int,yakl::memHost> intHost7d;
 
-typedef yakl::Array<bool,yakl::memDevice> umgBool1d;
-typedef yakl::Array<bool,yakl::memDevice> umgBool2d;
-typedef yakl::Array<bool,yakl::memDevice> umgBool3d;
-typedef yakl::Array<bool,yakl::memDevice> umgBool4d;
-typedef yakl::Array<bool,yakl::memDevice> umgBool5d;
-typedef yakl::Array<bool,yakl::memDevice> umgBool6d;
-typedef yakl::Array<bool,yakl::memDevice> umgBool7d;
+typedef yakl::FArray<bool,yakl::memDevice> umgBool1d;
+typedef yakl::FArray<bool,yakl::memDevice> umgBool2d;
+typedef yakl::FArray<bool,yakl::memDevice> umgBool3d;
+typedef yakl::FArray<bool,yakl::memDevice> umgBool4d;
+typedef yakl::FArray<bool,yakl::memDevice> umgBool5d;
+typedef yakl::FArray<bool,yakl::memDevice> umgBool6d;
+typedef yakl::FArray<bool,yakl::memDevice> umgBool7d;
 
-typedef yakl::Array<bool,yakl::memDevice> bool1d;
-typedef yakl::Array<bool,yakl::memDevice> bool2d;
-typedef yakl::Array<bool,yakl::memDevice> bool3d;
-typedef yakl::Array<bool,yakl::memDevice> bool4d;
-typedef yakl::Array<bool,yakl::memDevice> bool5d;
-typedef yakl::Array<bool,yakl::memDevice> bool6d;
-typedef yakl::Array<bool,yakl::memDevice> bool7d;
+typedef yakl::FArray<bool,yakl::memDevice> bool1d;
+typedef yakl::FArray<bool,yakl::memDevice> bool2d;
+typedef yakl::FArray<bool,yakl::memDevice> bool3d;
+typedef yakl::FArray<bool,yakl::memDevice> bool4d;
+typedef yakl::FArray<bool,yakl::memDevice> bool5d;
+typedef yakl::FArray<bool,yakl::memDevice> bool6d;
+typedef yakl::FArray<bool,yakl::memDevice> bool7d;
 
-typedef yakl::Array<bool,yakl::memHost> boolHost1d;
-typedef yakl::Array<bool,yakl::memHost> boolHost2d;
-typedef yakl::Array<bool,yakl::memHost> boolHost3d;
-typedef yakl::Array<bool,yakl::memHost> boolHost4d;
-typedef yakl::Array<bool,yakl::memHost> boolHost5d;
-typedef yakl::Array<bool,yakl::memHost> boolHost6d;
-typedef yakl::Array<bool,yakl::memHost> boolHost7d;
+typedef yakl::FArray<bool,yakl::memHost> boolHost1d;
+typedef yakl::FArray<bool,yakl::memHost> boolHost2d;
+typedef yakl::FArray<bool,yakl::memHost> boolHost3d;
+typedef yakl::FArray<bool,yakl::memHost> boolHost4d;
+typedef yakl::FArray<bool,yakl::memHost> boolHost5d;
+typedef yakl::FArray<bool,yakl::memHost> boolHost6d;
+typedef yakl::FArray<bool,yakl::memHost> boolHost7d;
 
 
 template <class T> YAKL_INLINE  T max(T v1, T v2) { return v1 > v2 ? v1 : v2; }
--- rrtmgp/kernels/mo_rrtmgp_util_reorder_kernels.cpp
+++ rrtmgp/kernels/mo_rrtmgp_util_reorder_kernels.cpp
@@ -2,13 +2,13 @@
 #include "mo_rrtmgp_util_reorder_kernels.h"
 
 extern "C" void reorder_123x321_kernel(int d1, int d2, int d3, real *array_in_p, real *array_out_p) {
-  umgReal3d array_in ( "array_in"  , array_in_p  , d3 , d2 , d1 );
-  umgReal3d array_out( "array_out" , array_out_p , d1 , d2 , d3 );
+  umgReal3d array_in ( "array_in"  , array_in_p  , d1 , d2 , d3 );
+  umgReal3d array_out( "array_out" , array_out_p , d3 , d2 , d1 );
 
-  for (int i3=0; i3<d3; i3++) {
-    for (int i2=0; i2<d2; i2++) {
-      for (int i1=0; i1<d1; i1++) {
-        array_out(i1,i2,i3) = array_in(i3,i2,i1);
+  for (int i3=1; i3<=d3; i3++) {
+    for (int i2=1; i2<=d2; i2++) {
+      for (int i1=1; i1<=d1; i1++) {
+        array_out(i3,i2,i1) = array_in(i1,i2,i3);
       }
     }
   }
