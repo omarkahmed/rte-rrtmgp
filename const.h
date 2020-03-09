71f629cdd3b0f70b40ce967561a5acac30d5eb19
Matt Norman <normanmr@ornl.gov>
2020-03-09 12:33:33 -0400

    Moving intrinsics to YAKL

--- const.h
+++ const.h
@@ -16,6 +16,11 @@ YAKL_INLINE real constexpr operator"" _wp( long double x ) {
 
 using yakl::fortran::parallel_for_cpu_serial;
 using yakl::fortran::Bounds;
+using yakl::max;
+using yakl::min;
+using yakl::abs;
+using yakl::fortran::merge;
+using yakl::fortran::size;
 
 
 typedef FArray<real,1,yakl::memDevice> umgReal1d;
@@ -89,14 +94,6 @@ typedef FArray<bool,4,yakl::memHost> boolHost4d;
 typedef FArray<bool,5,yakl::memHost> boolHost5d;
 typedef FArray<bool,6,yakl::memHost> boolHost6d;
 typedef FArray<bool,7,yakl::memHost> boolHost7d;
-
-
-template <class T> YAKL_INLINE  T max(T v1, T v2) { return v1 > v2 ? v1 : v2; }
-template <class T> YAKL_INLINE  T min(T v1, T v2) { return v1 < v2 ? v1 : v2; }
-
-template <class T> YAKL_INLINE  T merge(T t, T f, bool cond) { return cond ? t : f; }
-
-template <class T> YAKL_INLINE  int size(T arr, int dim) { return arr.dimension[dim-1]; }
 
 
 inline void stoprun( std::string str ) {
