7f7dae594dfcad3d013736aa09f5bd224efe40cc
Matt Norman <normanmr@ornl.gov>
2020-04-07 11:37:15 -0400

    cleaning up parallel_for

--- rte/kernels/mo_rte_solver_kernels.h
+++ rte/kernels/mo_rte_solver_kernels.h
@@ -38,7 +38,7 @@ YAKL_INLINE void sw_source_2str(int ncol, int nlay, int ngpt, bool top_at_1, rea
   if (top_at_1) {
     // do igpt = 1, ngpt
     //   do icol = 1, ncol
-    parallel_for_cpu_serial( Bounds<2>({1,ngpt},{1,ncol}) , YAKL_LAMBDA (int igpt, int icol) {
+    parallel_for_cpu_serial( Bounds<2>(ngpt,ncol) , YAKL_LAMBDA (int igpt, int icol) {
       for (int ilev=1; ilev<=nlay; ilev++) {
         source_up(icol,ilev,igpt)     =    Rdir(icol,ilev,igpt) * flux_dn_dir(icol,ilev,igpt);
         source_dn(icol,ilev,igpt)     =    Tdir(icol,ilev,igpt) * flux_dn_dir(icol,ilev,igpt);
@@ -53,7 +53,7 @@ YAKL_INLINE void sw_source_2str(int ncol, int nlay, int ngpt, bool top_at_1, rea
     // previous level is up (+1)
     // do igpt = 1, ngpt
     //   do icol = 1, ncol
-    parallel_for_cpu_serial( Bounds<2>({1,ngpt},{1,ncol}) , YAKL_LAMBDA (int igpt, int icol) {
+    parallel_for_cpu_serial( Bounds<2>(ngpt,ncol) , YAKL_LAMBDA (int igpt, int icol) {
       for (int ilev=nlay; ilev>=1; ilev--) {
         source_up(icol,ilev,igpt)   =    Rdir(icol,ilev,igpt) * flux_dn_dir(icol,ilev+1,igpt);
         source_dn(icol,ilev,igpt)   =    Tdir(icol,ilev,igpt) * flux_dn_dir(icol,ilev+1,igpt);
@@ -76,7 +76,7 @@ YAKL_INLINE void lw_transport_noscat(int ncol, int nlay, int ngpt, bool top_at_1
     // Top of domain is index 1
     // do igpt = 1, ngpt
     //   do icol = 1, ncol
-    parallel_for_cpu_serial( Bounds<2>({1,ngpt},{1,ncol}) , YAKL_LAMBDA (int igpt, int icol) {
+    parallel_for_cpu_serial( Bounds<2>(ngpt,ncol) , YAKL_LAMBDA (int igpt, int icol) {
       // Downward propagation
       for (int ilev=2; ilev<=nlay+1; ilev++) {
         radn_dn(icol,ilev,igpt) = trans(icol,ilev-1,igpt)*radn_dn(icol,ilev-1,igpt) + source_dn(icol,ilev-1,igpt);
@@ -94,7 +94,7 @@ YAKL_INLINE void lw_transport_noscat(int ncol, int nlay, int ngpt, bool top_at_1
     // Top of domain is index nlay+1
     // do igpt = 1, ngpt
     //   do icol = 1, ncol
-    parallel_for_cpu_serial( Bounds<2>({1,ngpt},{1,ncol}) , YAKL_LAMBDA (int igpt, int icol) {
+    parallel_for_cpu_serial( Bounds<2>(ngpt,ncol) , YAKL_LAMBDA (int igpt, int icol) {
       // Downward propagation
       for (int ilev=nlay; ilev>=1; ilev--) {
         radn_dn(icol,ilev,igpt) = trans(icol,ilev  ,igpt)*radn_dn(icol,ilev+1,igpt) + source_dn(icol,ilev,igpt);
@@ -134,7 +134,7 @@ YAKL_INLINE void sw_two_stream(int ncol, int nlay, int ngpt, real1d const &mu0, 
   // do igpt = 1, ngpt
   //   do ilay = 1, nlay
   //     do icol = 1, ncol
-  parallel_for_cpu_serial( Bounds<3>({1,ngpt},{1,nlay},{1,ncol}) , YAKL_LAMBDA (int igpt, int ilay, int icol) {
+  parallel_for_cpu_serial( Bounds<3>(ngpt,nlay,ncol) , YAKL_LAMBDA (int igpt, int ilay, int icol) {
     // Zdunkowski Practical Improved Flux Method "PIFM"
     //  (Zdunkowski et al., 1980;  Contributions to Atmospheric Physics 53, 147-66)
     real gamma1= (8._wp - w0(icol,ilay,igpt) * (5._wp + 3._wp * g(icol,ilay,igpt))) * .25_wp;
