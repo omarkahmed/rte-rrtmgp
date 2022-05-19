58b88e9f1e784362aad2c817fe9ceb5c6df74be6
Omar Khalil Ahmed <omar.ahmed@intel.com>
2022-05-19 16:19:14 -0700

    Adding scripts for building and running rte-rrmgp and running unit tests

---
+++ cpp/test/intel.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+module purge
+module load intel-nightly/20220516
+module load intel/mpi/2021.5.0
+
+export ARCH="SYCL"
+#export CXXFLAGS="-fsycl -sycl-target=spirv64_gen"
+export NCINCLUDE="`$HOME/e3sm-libs/oneapi-ifort/packages/netcdf-serial/bin/nc-config --includedir`"
+export NCFLAGS="`$HOME/e3sm-libs/oneapi-ifort/packages/netcdf-serial/bin/nc-config --libs`"
+export CC=icx
+export CXX=dpcpp
+#export CXX=icpx
+export FC=ifx
+export YAKLHOME="$HOME/e3sm-comps/YAKL"
